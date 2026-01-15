011e9b271b00ae89445a2514252b1fe860ee0b6a
Your Name <you@example.com>
2026-01-15 12:10:36 -0800

    added lerp color and changed formation of triangles

--- sketch.js
+++ sketch.js
@@ -1,23 +1,33 @@
+let from;
+let to;
+let interA;
+let interB;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  from = color(255, 0, 0);
+  to = color(0, 0, 0);
+
+  interA = lerpColor(from, to, 0.33);
+  interB = lerpColor(from, to, 0.66);
 }
 
 function draw() {
-  background(255, 0, 0);
-  myTriangle(.1,width/6, height/4);
-  myTriangle(.5, width/4, height/1);
-  myTriangle(.1, width/6, height/2);
+  background(255, 255, 255);
+  fill(from);
   myTriangle(.3, width/7, height/1);
-  
-
+  fill(interA);
+  myTriangle(.1, width/6, height/2);
+  fill(interB);
+  myTriangle(.1,width/6, height/4);
+  fill(to);
+  myTriangle(.3, width/4, height/1);
+}
   //console.log("mouseX: "+mouseX, "mouseY: "+mouseY);
-  
   function myTriangle (amountOfWindow, x, y){
     push();
     const size = width * amountOfWindow;
     translate(x,y);
     triangle(0, -size, size, size, -size, size);
     pop(); 
-
-  }
 }
